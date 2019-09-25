71e0ed5d4201090d63b4dbdb3c5cd65dc2492ba7
tiagopaes <tiagopaes37@gmail.com>
2019-09-25 17:00:45 -0300

    Add etherium icon

--- src/Utils/CoinIcons.js
+++ src/Utils/CoinIcons.js
@@ -1,3 +1,4 @@
 export const images = {
-  BTC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609483/bitcoin_eqld4v.png'
+  BTC: 'https://res.cloudinary.com/da7jhtpgh/image/upload/v1508609483/bitcoin_eqld4v.png',
+  ETH: 'https://icon-library.net/images/ethereum-icon/ethereum-icon-4.jpg'
 }
