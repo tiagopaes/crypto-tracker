c3770646adfaf55bde9a119210919f433341872b
tiagopaes <tiagopaes37@gmail.com>
2019-11-11 18:38:02 -0300

    create axios object

--- src/Actions/FetchCoinData.js
+++ src/Actions/FetchCoinData.js
@@ -6,28 +6,25 @@ import {
   FETCHING_COIN_DATA_FAIL,
 } from './../Utils/ActionTypes';
 
+const http = axios.create({
+  baseURL: apiBaseUrl,
+  headers: {
+    'X-CMC_PRO_API_KEY': apiSecretKey,
+  },
+});
+
 export default function FetchCoinData() {
   return async dispatch => {
     dispatch({type: FETCHING_COIN_DATA});
 
     try {
-      const response = await axios.get(
-        `${apiBaseUrl}/cryptocurrency/map?limit=10&sort=cmc_rank&aux=`,
-        {
-          headers: {
-            'X-CMC_PRO_API_KEY': apiSecretKey,
-          },
-        },
+      const response = await http.get(
+        '/cryptocurrency/map?limit=10&sort=cmc_rank&aux=',
       );
 
       const ids = response.data.data.map(item => item.id).toString();
-      const _response = await axios.get(
-        `${apiBaseUrl}/cryptocurrency/quotes/latest?id=${ids}`,
-        {
-          headers: {
-            'X-CMC_PRO_API_KEY': apiSecretKey,
-          },
-        },
+      const _response = await http.get(
+        `/cryptocurrency/quotes/latest?id=${ids}`,
       );
 
       return dispatch({
