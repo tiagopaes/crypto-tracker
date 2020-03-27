c79ec1812417f05e168980f7f4caf9f669718a5c
tiagopaes <tiagopaes37@gmail.com>
2020-03-26 23:25:05 -0300

    add react-native-dotenv

--- src/Utils/Contants.js
+++ src/Utils/Contants.js
@@ -1,2 +1,4 @@
-export const apiBaseUrl = 'https://pro-api.coinmarketcap.com/v1';
-export const apiSecretKey = 'c9b26bba-a17d-446f-857b-44a9245d76fe';
+import { API_BASE_URL, API_SECRET_KEY } from 'react-native-dotenv'
+
+export const apiBaseUrl = API_BASE_URL;
+export const apiSecretKey = API_SECRET_KEY;
